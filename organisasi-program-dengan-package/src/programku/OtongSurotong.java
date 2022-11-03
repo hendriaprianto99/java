d2d0f47c8ce88bddec96679791caaf08e2695334
hendriaprianto99 <hendriaprianto99@gmail.com>
2022-11-04 00:50:54 +0700

    Menambahkan organisasi-program-dengan-package

---
+++ organisasi-program-dengan-package/src/programku/OtongSurotong.java
@@ -0,0 +1,6 @@
+package programku;
+public class OtongSurotong {
+    public static void main(String[] args) {
+        System.out.println("Namaku adalah Otong Surotong, temannya si Ucup.");
+    }
+}
